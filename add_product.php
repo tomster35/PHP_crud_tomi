cb8e5a0fe6d325d79813edad7857ebf945c60b5c
sebgates <sebgates@gmail.com>
2019-02-13 16:02:15 +0000

    Initial Commit for all project files

---
+++ add_product.php
@@ -0,0 +1,80 @@
+<?php
+
+// Get the product data
+$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+$code = filter_input(INPUT_POST, 'code');
+$name = filter_input(INPUT_POST, 'name');
+$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
+
+// Validate inputs
+if ($category_id == null || $category_id == false ||
+        $code == null || $name == null || $price == null || $price == false) {
+    $error = "Invalid product data. Check all fields and try again.";
+    include('error.php');
+    exit();
+} else {
+
+    /**************************** Image upload ****************************/
+
+    error_reporting(~E_NOTICE); 
+
+// avoid notice
+
+    $imgFile = $_FILES['product_image']['name'];
+    $tmp_dir = $_FILES['product_image']['tmp_name'];
+    echo $_FILES['product_image']['tmp_name'];
+    $imgSize = $_FILES['product_image']['size'];
+
+    if (empty($imgFile)) {
+        $product_image = "";
+    } else {
+        $upload_dir = 'image_uploads/'; // upload directory
+
+        $imgExt = strtolower(pathinfo($imgFile, PATHINFO_EXTENSION)); // get image extension
+        // valid image extensions
+        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
+        // rename uploading image
+        $product_image = rand(1000, 1000000) . "." . $imgExt;
+        // allow valid image file formats
+        if (in_array($imgExt, $valid_extensions)) {
+        // Check file size '5MB'
+            if ($imgSize < 5000000) {
+                if (move_uploaded_file($_FILES["product_image"]["tmp_name"], $upload_dir . $product_image)) {
+                    echo "The file ". basename( $_FILES["product_image"]["name"]). " has been uploaded.";
+                } else {
+                    $error =  "Sorry, there was an error uploading your file.";
+                    include('error.php');
+                    exit();
+                }
+            } else {
+                $error = "Sorry, your file is too large.";
+                include('error.php');
+                exit();
+            }
+        } else {
+            $error = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            include('error.php');
+            exit();
+        }
+    }
+/******************** End Image upload *******************/
+    
+    require_once('database.php');
+
+    // Add the product to the database 
+    $query = "INSERT INTO products
+                 (categoryID, productCode, productName, listPrice, productImage)
+              VALUES
+                 (:category_id, :code, :name, :price, :product_image)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->bindValue(':code', $code);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':product_image', $product_image);
+    $statement->execute();
+    $statement->closeCursor();
+
+    // Display the Product List page
+    include('index.php');
+}
---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+require_once('database.php');
+
+// Get category ID
+if (!isset($category_id)) {
+$category_id = filter_input(INPUT_GET, 'category_id', 
+FILTER_VALIDATE_INT);
+if ($category_id == NULL || $category_id == FALSE) {
+$category_id = 1;
+}
+}
+
+// Get name for current category
+$queryCategory = "SELECT * FROM categories
+WHERE categoryID = :category_id";
+$statement1 = $db->prepare($queryCategory);
+$statement1->bindValue(':category_id', $category_id);
+$statement1->execute();
+$category = $statement1->fetch();
+$statement1->closeCursor();
+$category_name = $category['categoryName'];
+
+// Get all categories
+$queryAllCategories = 'SELECT * FROM categories
+ORDER BY categoryID';
+$statement2 = $db->prepare($queryAllCategories);
+$statement2->execute();
+$categories = $statement2->fetchAll();
+$statement2->closeCursor();
+
+// Get products for selected category
+$queryProducts = "SELECT * FROM products
+WHERE categoryID = :category_id
+ORDER BY productID";
+$statement3 = $db->prepare($queryProducts);
+$statement3->bindValue(':category_id', $category_id);
+$statement3->execute();
+$products = $statement3->fetchAll();
+$statement3->closeCursor();
+?>
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+<title>My Guitar Shop</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Product Manager</h1></header>
+<main>
+<h1>Product List</h1>
+
+<aside>
+<!-- display a list of categories -->
+<h2>Categories</h2>
+<nav>
+<ul>
+<?php foreach ($categories as $category) : ?>
+<li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
+<?php echo $category['categoryName']; ?>
+</a>
+</li>
+<?php endforeach; ?>
+</ul>
+</nav>          
+</aside>
+
+<section>
+<!-- display a table of products -->
+<h2><?php echo $category_name; ?></h2>
+<table>
+<tr>
+<th>Image</th>
+<th>Name</th>
+<th>Price</th>
+<th>Delete</th>
+<th>Edit</th>
+</tr>
+<?php foreach ($products as $product) : ?>
+<tr>
+<td><img src="image_uploads/<?php echo $product['productImage']; ?>" width="100px" height="100px" /></td>
+<td><?php echo $product['productName']; ?></td>
+<td class="right"><?php echo $product['listPrice']; ?></td>
+<td><form action="delete_product.php" method="post"
+id="delete_product_form">
+<input type="hidden" name="product_id"
+value="<?php echo $product['productID']; ?>">
+<input type="hidden" name="category_id"
+value="<?php echo $product['categoryID']; ?>">
+<input type="submit" value="Delete">
+</form></td>
+<td><form action="edit_product_form.php" method="post"
+id="delete_product_form">
+<input type="hidden" name="product_id"
+value="<?php echo $product['productID']; ?>">
+<input type="hidden" name="category_id"
+value="<?php echo $product['categoryID']; ?>">
+<input type="submit" value="Edit">
+</form></td>
+</tr>
+<?php endforeach; ?>
+</table>
+<p><a href="add_product_form.php">Add Product</a></p>
+<p><a href="category_list.php">List Categories</a></p>
+</section>
+</main>
+
+<footer>
+<p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+</footer>
+</body>
+</html>
